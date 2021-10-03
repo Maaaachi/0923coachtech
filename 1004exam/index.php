2df39948e6104a3c35dcf99df6e385cb646e7800
Machi <maaaachi0322@docomo.ne.jp>
2021-10-04 08:59:59 +0900

    Create index.php

---
+++ 1004exam/index.php
@@ -0,0 +1,12 @@
+<table>
+  <?php
+  for ($a = 1; $a <=9; ) {
+    print '<tr>';
+    for ($b = 1; $a <= 9;){
+      print '<td>' . $a * $b . '</tb>';
+    }
+    print '</tr>';
+  }
+  ?>
+  </table>
+  
